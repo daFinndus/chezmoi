5997852a6603a8b789ebce39237081beed6c15d5
daFinndus <finnlucajensen@proton.me>
2026-03-17 21:26:58 +0100

    feat: Update rmshit script

--- dot_config/scripts/executable_rmshit.py
+++ dot_config/scripts/executable_rmshit.py
@@ -4,6 +4,7 @@ import os
 import sys
 import yaml
 import shutil
+import subprocess
 
 from pathlib import Path
 
@@ -13,94 +14,123 @@ from pathlib import Path
 # It is modified though
 
 DEFAULT_CONFIG = """
-- ~/.adobe                           # Flash crap
-- ~/.macromedia                      # Flash crap
-- ~/.recently-used                   # Recently used files
-- ~/.local/share/recently-used.xbel  # Recently used files
-- ~/.thumbnails                      # Image thumbnails cache
-- ~/.gconfd                          # GNOME configuration daemon
-- ~/.gconf                           # GNOME configuration
-- ~/.local/share/gegl-0.2            # GEGL library cache
-- ~/.FRD/log/app.log                 # FRD
 - ~/.FRD/links.txt                   # FRD
-- ~/.objectdb                        # FRD
-- ~/.gstreamer-0.10                  # GStreamer cache
-- ~/.pulse                           # PulseAudio
-- ~/.esd_auth                        # ESD authentication
-- ~/.config/enchant                  # Spell checker cache
-- ~/.spicec                          # Contains only log file
-- ~/.dropbox-dist                    # Dropbox distribution files
-- ~/.parallel                        # GNU Parallel
-- ~/.dbus                            # D-Bus session files
-- ~/ca2                              # WTF
-- ~/ca2~                             # WTF
-- ~/.distlib/                        # Contains another empty dir
+- ~/.FRD/log/app.log                 # FRD
+- ~/.QtWebEngineProcess/             # Qt WebEngine cache
+- ~/.adobe                           # Flash crap
+- ~/.ansible/                        # Ansible cache
+- ~/.asy/                            # Asymptote cache
 - ~/.bazaar/                         # Bzr insists on creating files
+- ~/.bundle/cache/                   # Ruby Bundle cache
 - ~/.bzr.log                         # Bazaar log file
-- ~/.nv/                             # NVIDIA cache
-- ~/.viminfo                         # Sometimes created wrongfully
-- ~/.npm/                            # NPM cache
+- ~/.cabal/logs/                     # Haskell Cabal logs
+- ~/.cache/JetBrains/                # JetBrains cache
+- ~/.cache/bazel/                    # Bazel build cache
+- ~/.cache/chromium/                 # Chromium cache
+- ~/.cache/containers/               # Containers cache
+- ~/.cache/deno/                     # Deno cache
+- ~/.cache/electron/                 # Electron cache
+- ~/.cache/fontconfig/               # Font cache
+- ~/.cache/go-build/                 # Go build cache
+- ~/.cache/google-chrome/            # Google Chrome cache
+- ~/.cache/helm/                     # Helm cache
+- ~/.cache/librewolf/                # LibreWolf cache
+- ~/.cache/mesa_shader_cache/        # Mesa shader cache
+- ~/.cache/mesa_shader_cache_db/     # Mesa shader cache DB
+- ~/.cache/mozilla/                  # Mozilla cache
+- ~/.cache/ms-playwright/            # Playwright cache
+- ~/.cache/nvim/                     # Neovim cache
+- ~/.cache/obs-studio/               # OBS cache
+- ~/.cache/paru/                     # Paru cache
+- ~/.cache/pip/                      # Pip cache
+- ~/.cache/pnpm/                     # PNPM package manager cache
+- ~/.cache/podman/                   # Podman cache
+- ~/.cache/pre-commit/               # pre-commit cache
+- ~/.cache/pypoetry/                 # Poetry cache
+- ~/.cache/spotify/                  # Spotify cache
+- ~/.cache/thumbnails/               # Thumbnail cache
+- ~/.cache/typescript/               # TypeScript cache
+- ~/.cache/yarn/                     # Yarn package manager cache
+- ~/.cache/yay/                      # Yay AUR helper cache
+- ~/.cache/zed/                      # Zed cache
+- ~/.cmake/                          # CMake cache
+- ~/.composer/cache/                 # PHP Composer cache
+- ~/.config/Code/Cache/              # VSCode cache
+- ~/.config/Code/CachedData/         # VSCode cached data
+- ~/.config/Code/Service Worker/CacheStorage/   # VSCode service worker cache
+- ~/.config/VSCodium/Cache/          # VSCodium cache
+- ~/.config/VSCodium/CachedData/     # VSCodium cached data
+- ~/.config/discord/Cache/           # Discord cache
+- ~/.config/discord/Code Cache/      # Discord code cache
+- ~/.config/discord/GPUCache/        # Discord GPU cache
+- ~/.config/enchant                  # Spell checker cache
+- ~/.config/vesktop/Cache/           # Vesktop cache
+- ~/.config/vesktop/Code Cache/      # Vesktop code cache
+- ~/.config/vesktop/GPUCache/        # Vesktop GPU cache
+- ~/.cpan/build/                     # CPAN build cache
+- ~/.dbus                            # D-Bus session files
+- ~/.distlib/                        # Contains another empty dir
+- ~/.dropbox-dist                    # Dropbox distribution files
+- ~/.electron-gyp/                   # Electron native addon build cache
+- ~/.esd_auth                        # ESD authentication
+- ~/.fltk/                           # FLTK cache
+- ~/.gconf                           # GNOME configuration
+- ~/.gconfd                          # GNOME configuration daemon
+- ~/.gem/specs/                      # Ruby Gem specs cache
+- ~/.gnome/                          # GNOME cache
+- ~/.go/pkg/mod/cache/               # Go module cache
+- ~/.gradle/caches/                  # Gradle build cache
+- ~/.gstreamer-0.10                  # GStreamer cache
+- ~/.ivy2/cache/                     # Ivy cache
 - ~/.java/                           # Java cache and temp files
-- ~/.swt/                            # Standard Widget Toolkit cache
-- ~/.oracle_jre_usage/               # Oracle JRE usage data
+- ~/.jssc/                           # Java Simple Serial Connector
+- ~/.local/share/Steam/logs/         # Steam log files
+- ~/.local/share/Trash/              # Trash
+- ~/.local/share/gegl-0.2            # GEGL library cache
+- ~/.local/share/recently-used.xbel  # Recently used files
+- ~/.local/share/vulkan/             # Vulkan cache
+- ~/.lesshst                         # less history
+- ~/.macromedia                      # Flash crap
+- ~/.mozilla/firefox/*/cache2/       # Firefox cache
+- ~/.mozilla/firefox/*/startupCache/ # Firefox startup cache
+- ~/.node-gyp/                       # Node.js native addon build cache
+- ~/.npm/                            # NPM cache
+- ~/.npm/_cacache/                   # NPM content-addressable cache
+- ~/.nv/                             # NVIDIA cache
+- ~/.nvm/.cache/                     # Node Version Manager cache
+- ~/.objectdb                        # FRD
 - ~/.openjfx/                        # OpenJFX cache
+- ~/.oracle_jre_usage/               # Oracle JRE usage data
 - ~/.org.jabref.gui.JabRefMain/      # JabRef cache
 - ~/.org.jabref.gui.MainApplication/ # JabRef cache
-- ~/.jssc/                           # Java Simple Serial Connector
-- ~/.tox/                            # Cache directory for tox
-- ~/.pylint.d/                       # Pylint cache
-- ~/.qute_test/                      # Qutebrowser test files
-- ~/.QtWebEngineProcess/             # Qt WebEngine cache
-- ~/.qutebrowser/                    # Created empty
-- ~/.asy/                            # Asymptote cache
-- ~/.cmake/                          # CMake cache
-- ~/.gnome/                          # GNOME cache
-- ~/unison.log                       # Unison log file
-- ~/.texlive/                        # TeX Live cache
-- ~/.w3m/                            # w3m browser cache
-- ~/.subversion/                     # Subversion cache
-- ~/nvvp_workspace/                  # Created empty
-- ~/.ansible/                        # Ansible cache
-- ~/.fltk/                           # FLTK cache
-- ~/.vnc/                            # VNC cache
-- ~/.local/share/Trash/              # VSCode puts deleted files here
-- ~/.cache/yay/                      # Yay AUR helper cache
+- ~/.parallel                        # GNU Parallel
 - ~/.pip/cache/                      # Pip cache (old location)
-- ~/.cache/pip/                      # Pip cache
-- ~/.node-gyp/                       # Node.js native addon build cache
-- ~/.electron-gyp/                   # Electron native addon build cache
-- ~/.cache/electron/                 # Electron cache
-- ~/.cache/chromium/                 # Chromium cache
-- ~/.cache/google-chrome/            # Google Chrome cache
-- ~/.cache/mozilla/                  # Mozilla/Firefox cache
-- ~/.cache/mesa_shader_cache/        # Mesa shader cache
-- ~/.cache/fontconfig/               # Font cache
-- ~/.cache/thumbnails/               # Thumbnail cache
-- ~/.gradle/caches/                  # Gradle build cache
-- ~/.sbt/boot/                       # SBT boot cache
-- ~/.ivy2/cache/                     # Ivy cache
-- ~/.m2/repository/                  # Maven repository cache
-- ~/.cargo/registry/cache/           # Rust Cargo cache
-- ~/.go/pkg/mod/cache/               # Go module cache
-- ~/.nuget/packages/                 # NuGet package cache
-- ~/.dotnet/                         # .NET Core cache
+- ~/.pulse                           # PulseAudio
+- ~/.pylint.d/                       # Pylint cache
+- ~/.python_history                  # Python REPL history
+- ~/.qute_test/                      # Qutebrowser test files
+- ~/.qutebrowser/                    # Created empty
+- ~/.recently-used                   # Recently used files
+- ~/.rediscli_history                # Redis CLI history
 - ~/.rustup/tmp/                     # Rustup temporary files
-- ~/.cache/yarn/                     # Yarn package manager cache
-- ~/.cache/pnpm/                     # PNPM package manager cache
-- ~/.cache/go-build/                 # Go build cache
-- ~/.cache/bazel/                    # Bazel build cache
-- ~/.wine/drive_c/windows/Temp/      # Wine temporary files
+- ~/.sbt/boot/                       # SBT boot cache
+- ~/.spicec                          # Contains only log file
+- ~/.sqlite_history                  # SQLite history
 - ~/.steam/logs/                     # Steam log files
-- ~/.local/share/Steam/logs/         # Steam log files
-- ~/.local/share/vulkan/             # Vulkan cache
-- ~/.nvm/.cache/                     # Node Version Manager cache
-- ~/.composer/cache/                 # PHP Composer cache
-- ~/.bundle/cache/                   # Ruby Bundle cache
-- ~/.gem/specs/                      # Ruby Gem specs cache
-- ~/.cpan/build/                     # CPAN build cache
-- ~/.cabal/logs/                     # Haskell Cabal logs
-- ~/.stack/programs/                 # Haskell Stack programs cache
-- ~/.cache/yay/                      # Yay AUR helper cache
+- ~/.swt/                            # Standard Widget Toolkit cache
+- ~/.texlive/                        # TeX Live cache
+- ~/.thumbnails                      # Image thumbnails cache
+- ~/.tox/                            # Cache directory for tox
+- ~/.var/app/*/cache/                # Flatpak app caches
+- ~/.viminfo                         # Sometimes created wrongfully
+- ~/.vnc/                            # VNC cache
+- ~/.w3m/                            # w3m browser cache
+- ~/.wget-hsts                       # wget HSTS cache
+- ~/.wine/drive_c/windows/Temp/      # Wine temporary files
+- ~/ca2                              # WTF
+- ~/ca2~                             # WTF
+- ~/nvvp_workspace/                  # Created empty
+- ~/unison.log                       # Unison log file
 """
 
 CONFIG_PATH = Path(os.getenv("XDG_CONFIG_HOME", Path.home() / ".config")) / "rmshit.yaml"
@@ -111,11 +141,13 @@ def format_size(size: float) -> str:
         if size < 1024:
             return f"{size:.1f} {unit}"
         size /= 1024
+    
     return f"{size:.1f} PiB"
 
 # Get directory size function to calculate total size of files in a directory
 def get_dir_size(path: Path) -> int:
     total = 0
+    
     try:
         if path.is_file():
             return path.stat().st_size
@@ -127,6 +159,7 @@ def get_dir_size(path: Path) -> int:
                 continue
     except Exception:
         pass
+    
     return total
 
 # Load configuration function to read paths from the config file 
@@ -144,12 +177,37 @@ def load_config() -> list[Path]:
 
 # Function to prompt user for yes/no input
 def yesno(question: str, default="n") -> bool:
-    prompt = f"{question} (y/[n]) " if default == "n" else f"{question} ([y]/n) "
+    prompt = f"\n{question} (y/[n]) " if default == "n" else f"{question} ([y]/n) "
     ans = input(prompt).strip().lower()
+    
     if not ans:
         ans = default
+    
     return ans.startswith("y")
 
+# This is a helper for cleaning up via a command
+def run_cleanup_command(name: str, command: list[str], path_to_measure: Path | None = None) -> int:
+    before = get_dir_size(path_to_measure) if path_to_measure else 0
+
+    print(f"\nRunning {name}...")
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        print(f"Failed to run {name}:")
+
+        if result.stderr.strip():
+            print(result.stderr.strip())
+        
+        return 0
+
+    after = get_dir_size(path_to_measure) if path_to_measure else 0
+    freed = max(0, before - after)
+
+    print(f"{name} freed {format_size(freed)}.")
+
+    return freed
+
+# This is the main function
 def rmshit():
     junk_paths = load_config()
     found = []
@@ -170,8 +228,10 @@ def rmshit():
         return
 
     print("\nFound junk files/directories:")
+    
     for path, size in found:
         print(f"  {path}  ({format_size(size)})")
+
     print(f"\nTotal size: {format_size(total_size)}")
 
     if not yesno("Remove all?", default="n"):
@@ -179,6 +239,7 @@ def rmshit():
         return
 
     freed = 0
+
     for path, size in found:
         try:
             if path.is_file() or path.is_symlink():
@@ -189,7 +250,30 @@ def rmshit():
         except Exception as e:
             print(f"Failed to delete {path}: {e}")
 
-    print(f"\nCleaned up {format_size(freed)} in total.")
+    print("\nNow clearing pacman caches.")
+
+    if yesno("Run paccache cleanup too?", default="n"):
+        freed += run_cleanup_command(
+            "paccache -r",
+            ["sudo", "paccache", "-r"],
+            Path("/var/cache/pacman/pkg"),
+        )
+
+    if yesno("Remove uninstalled pacman cache too?", default="n"):
+        freed += run_cleanup_command(
+            "paccache -ruk0",
+            ["sudo", "paccache", "-ruk0"],
+            Path("/var/cache/pacman/pkg"),
+        )
+
+    if yesno("Vacuum journal logs older than 7 days?", default="n"):
+        freed += run_cleanup_command(
+            "journalctl --vacuum-time=7d",
+            ["sudo", "journalctl", "--vacuum-time=7d"],
+            Path("/var/log/journal"),
+        )
+
+    print(f"\nTotal freed overall: {format_size(freed)}")
 
 if __name__ == "__main__":
     rmshit()
