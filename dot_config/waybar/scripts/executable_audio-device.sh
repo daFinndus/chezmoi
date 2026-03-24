2feb7428be32d774b27e9729c9ddb78db63cdd25
daFinndus <finnlucajensen@proton.me>
2026-03-24 22:08:04 +0100

    feat: Add toggle volume script and mute button

---
+++ dot_config/waybar/scripts/executable_audio-device.sh
@@ -0,0 +1,20 @@
+DEVICES=($(pactl list sinks | grep -i "node.name" | awk '{print $3}' | tr -d '"'))
+CURRENT=$(pactl get-default-sink)
+
+log() {
+  echo "[AUDIO] $1"
+}
+
+if [[ "${#DEVICES[@]}" -gt 2 ]]; then
+  log "Aborting script, too many devices registered."
+fi
+
+for device in "${DEVICES[@]}"; do
+  if [[ "$device" == "$CURRENT" ]]; then
+    log "Skipping "$device", it's currently default..."
+    continue
+  else
+    log "Setting $device as default sink!"
+    pactl set-default-sink $device
+  fi
+done
