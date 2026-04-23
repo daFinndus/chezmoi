1adb5d5e00b148df94d3b8e2abbbcbd6ea4ca42d
daFinndus <finnlucajensen@proton.me>
2026-04-23 12:29:32 +0200

    feat: Add duplicati script, remove ewwbar script

---
+++ dot_config/scripts/executable_duplicati.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# This script is used to run specific duplicati backups.
+
+RED="\033[38;2;220;50;50m"
+YELLOW="\033[38;2;220;180;0m"
+GREEN="\033[38;2;50;200;50m"
+BLUE="\033[38;2;50;120;220m"
+RESET="\033[0m"
+
+log_step() { echo -e "\n${BLUE}[*]${RESET} $1"; }
+log_success() { echo -e "${GREEN}[+]${RESET} $1"; }
+log_warn() { echo -e "${YELLOW}[!]${RESET} $1"; }
+log_error() { echo -e "${RED}[-]${RESET} $1"; }
+
+PASSWORD=$1
+TOKEN=""
+
+# This is to curl the auth token.
+curl_auth_token() {
+  log_step "Going to curl access token..."
+
+  if [[ -z "$PASSWORD" ]]; then
+    log_error "Provided no password, aborting!"
+    exit 1
+  else
+    log_success "Got password: $PASSWORD"
+
+    RESPONSE=$(curl -s -X POST http://192.168.0.64:8200/api/v1/auth/login \
+      -H "Content-Type: application/json" \
+      -d "{\"Password\": \"$PASSWORD\"}")
+
+    TOKEN=$(echo "$RESPONSE" | jq -r '.AccessToken')
+
+    if [[ "$TOKEN" != "null" ]]; then
+      log_success "Got token!"
+    else
+      log_error "Couldn't retrieve token."
+      exit 1
+    fi
+  fi
+}
+
+IDS=()
+NAMES=()
+
+# This is to retrieve current backups ids
+get_backup_ids() {
+  log_step "Going to retrieve backups."
+
+  RESPONSE=$(curl -s http://192.168.0.64:8200/api/v1/backups \
+    -H "Authorization: Bearer $TOKEN")
+
+  IDS=$(echo "$RESPONSE" | jq -r '.[].Backup.ID')
+  NAMES=$(echo "$RESPONSE" | jq -r '.[].Backup.Name')
+
+  mapfile -t IDS <<<"$IDS"
+  mapfile -t NAMES <<<"$NAMES"
+
+  if [[ "${#IDS[@]}" -gt 0 ]]; then
+    log_success "Found the following backup id's:\n"
+
+    for ((i = 0; i < ${#IDS[@]}; i++)); do
+      echo "${IDS[i]} : ${NAMES[i]}"
+    done
+  else
+    log_warn "Didn't find any backups... aborting."
+    exit 0
+  fi
+}
+
+# This function will backup everything.
+# Maybe update this to do specific backups later.
+do_backups() {
+  log_step "Going to do backups..."
+
+  for ((i = 0; i < ${#IDS[@]}; i++)); do
+    log_success "\nGoing to run update for backup: ${NAMES[i]}"
+
+    RESPONSE=$(curl -s -X POST http://192.168.0.64:8200/api/v1/backup/${IDS[i]}/run \
+      -H "Authorization: Bearer $TOKEN")
+
+    if [[ "$RESPONSE" == *"OK"* ]]; then
+      log_success "Authorized backup for ${NAMES[i]}."
+    else
+      log_error "Backup progress failed for ${NAMES[i]}."
+    fi
+
+    sleep 3
+  done
+
+  log_success "Successfully did all updates!"
+}
+
+curl_auth_token
+get_backup_ids
+do_backups
