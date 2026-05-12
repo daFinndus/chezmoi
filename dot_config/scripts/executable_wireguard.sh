fe0960615919f79c54be4e5be8e323deb04233ab
daFinndus <finnlucajensen@proton.me>
2026-05-12 13:04:32 +0200

    fix: Fix vpn-manager script for waybar

--- dot_config/scripts/executable_wireguard.sh
+++ dot_config/scripts/executable_wireguard.sh
@@ -1,19 +1,29 @@
 CONNECTED=$(sudo wg show)
 
 log() {
-  echo "[WIREGUARD] $1"
+	echo "[WIREGUARD] $1"
 }
 
 if [[ -z "$CONNECTED" ]]; then
-  log "Seems you are not connected to home network..."
-  sudo wg-quick up kabuki
+	log "Seems you are not connected to home network..."
+	sudo wg-quick up kabuki
 
-  notify-send "Wireguard" "Now connected to home network"
-  log "Now connected to home VPN."
+	if [[ $(echo $? -eq 0) ]]; then
+		notify-send "Wireguard" "Now connected to home network"
+		log "Now connected to home VPN."
+	else
+		notify-send "Wireguard" "Something went wrong!"
+		log "Couldn't connect to home network."
+	fi
 else
-  log "Seems wireguard is already up and running!"
-  sudo wg-quick down kabuki
+	log "Seems wireguard is already up and running!"
+	sudo wg-quick down kabuki
 
-  notify-send "Wireguard" "Disconnected from home network"
-  log "Closed connection now."
+	if [[ $(echo $? -eq 0) ]]; then
+		notify-send "Wireguard" "Disconnected from home network"
+		log "Closed connection now."
+	else
+		notify-send "Wireguard" "Something went wrong!"
+		log "Couldn't disconnect from home network."
+	fi
 fi
