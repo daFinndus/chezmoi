c8db016179b5a812b820f0c0956f5a63b8cfb8a6
daFinndus <finnlucajensen@proton.me>
2026-05-19 12:52:48 +0200

    feat: Replace waybar with quickshell

--- dot_config/hypr/autostart.lua
+++ dot_config/hypr/autostart.lua
@@ -3,7 +3,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("hypridle")
 
-    hl.exec_cmd("waybar")
+    hl.exec_cmd("quickshell")
     hl.exec_cmd("dunst")
 
     -- Get active wallpaper on boot
