11126bdad814e4f6cb57bf3ec44b8d0d04e38943
daFinndus <finnlucajensen@proton.me>
2026-07-12 17:15:10 +0200

    feat: Adjust scripts

--- dot_config/scripts/executable_hyprlock-fix.sh
+++ dot_config/scripts/executable_hyprlock-fix.sh
@@ -1,24 +1,24 @@
-!#/bin/bash
+#!/usr/bin/env bash
 
 log() {
   echo "[LOCKFIX] $1"
 }
 
-exec() {
+execute() {
   sleep 2
 
-  hyprctl --instance 0 $1
+  hyprctl --instance 0 "$1" "$2"
 
   sleep 1
 }
 
 log "Going to enable session lock restore..."
 log "Returning 'ok's are a good thing."
-exec "eval \"hl.config({ misc = { allow_session_lock_restore = true }})\""
+execute eval "hl.config({ misc = { allow_session_lock_restore = true }})"
 
 log "Now restarting hyprlock!"
-exec "eval \"hl.dispatch(hl.dsp.exec_cmd('hyprlock'))\""
+execute eval "hl.dispatch(hl.dsp.exec_cmd('hyprlock'))"
 
 log "Now going to enable dpms."
-exec "dispatch \"hl.dsp.dpms({ action = 'enable' })\""
+execute dispatch "hl.dsp.dpms({ action = 'enable' })"
 
